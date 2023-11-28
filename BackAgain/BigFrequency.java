a92f6266bd61fef108af13f5298cbd18e3a99358
Nikhil Karmakar <99026264+Karnikhil90@users.noreply.github.com>
2023-11-28 10:53:45 +0000

    Update the new file and finish it half of it and soon gonna do it complete

--- BackAgain/BigFrequency.java
+++ BackAgain/BigFrequency.java
@@ -5,47 +5,67 @@
 // 4.the special also calculated
 // ----------------START----------------
 import java.util.Scanner;
-public class BigFrequency{
-  Scanner input;
-  short spaces, capital, small, number, special;
-  // Constructor 
-  BigFrequency(){
-    input = new Scanner(System.in);
-  }
-  // Get the word
-  public String gettheword(){
-    System.out.println("Enter the line");
-    return input.nextLine();
-  }
-  public void Process(String line)
-  {
-    short lengthOfLine = line.length();
-      for(int alpha = 0; alpha <= lengthOfLine;alpha++)
-      {
-        char ch = line.charAt(alpha);
-        
-        if(Character.isUpperCase(ch))
-        {
-          capital++;
-        }else if (Character.isLowerCase(ch))
-        {
-          small++;
-        }else if(Character.isWhiteSpace(ch)){
-          space++;
+public class BigFrequency {
 
-        }else if(Character.isDigit(ch))
-        {  
-          number++;
-        }
-        else{
-            special++;
-        }
-      }
-  }
+    Scanner input;
+    short spaces, capital, small, number, special;
 
-  
-  public static void main(String[] args){
-    Frequency myObj = new Frequency();
-    myObj.Process(myObj.gettheword());
-  }
+    // Constructor to initialize the Scanner object
+    public BigFrequency() {
+        input = new Scanner(System.in);
+    }
+
+    // Method to get the input line from the user
+    public String InputLine() {
+        System.out.println("Enter the line: ");
+        return input.nextLine();
+    }
+
+    // Method to process the input line and count the frequency of different character types
+    public void Process(String line) {
+        int lengthOfLine = line.length();
+
+        // Iterate through each character in the line
+        for (int alpha = 0; alpha < lengthOfLine; alpha++) { // Corrected loop condition
+            char ch = line.charAt(alpha);
+
+            // Check if the character is a capital letter
+            if (Character.isUpperCase(ch)) {
+                capital++;
+            }
+
+            // Check if the character is a small letter
+            else if (Character.isLowerCase(ch)) {
+                small++;
+            }
+
+            // Check if the character is a whitespace character (space)
+            else if (Character.isWhitespace(ch)) {
+                spaces++;
+            }
+
+            // Check if the character is a numeric character (number)
+            else if (Character.isDigit(ch)) {
+                number++;
+            }
+
+            // Count any other character as a special character
+            else {
+                special++;
+            }
+        }
+        System.out.println("Capital letters: " + capital);
+        System.out.println("Small letters: " + small);
+        System.out.println("Special letters: " + special);
+        System.out.println("Spaces: " + spaces);
+        System.out.println("Numbers: " + number);
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        // Print the frequency of different character types
+        BigFrequency myObj = new BigFrequency(); // Create an instance of the BigFrequency class
+        myObj.Process(myObj.InputLine()); // Process the input line
+
+    }
 }
